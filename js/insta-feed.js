89cf7339e808bd41469806b2dca63c3af43198c4
Matt Oskamp <mattoskamp@gmail.com>
2017-04-06 15:54:36 -0400

    instafeed now links to actual picture

--- js/insta-feed.js
+++ js/insta-feed.js
@@ -6,7 +6,7 @@ function getInsta() {
         accessToken: '4375800212.dccb08c.c781221c063b41ac9e9d03d519d06614',
         resolution: 'low_resolution',
         limit: 12,
-        template: '<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6"><img src="{{image}}" class="img-responsive" alt=""></div>'
+        template: '<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-responsive" alt=""></a></div>'
     });
     feed.run();
 }
